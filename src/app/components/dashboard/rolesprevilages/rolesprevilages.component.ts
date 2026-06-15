b296f357525ce2d5f86362f4cd0ba3910c2f896a
gkprasad27 <krishnaprasadg81@gmail.com>
2026-06-15 16:58:15 +0530

    changes

--- src/app/components/dashboard/rolesprevilages/rolesprevilages.component.ts
+++ src/app/components/dashboard/rolesprevilages/rolesprevilages.component.ts
@@ -11,7 +11,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { CommonService } from '../../../services/common.service';
 import { ActivatedRoute } from '@angular/router';
 
-import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -19,20 +19,46 @@ import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-rolesprevilages',
-  imports: [FormsModule, ReactiveFormsModule, MatButtonModule, TranslateModule, MatPaginator, MatTableModule, MatCardModule, MatSelectModule, MatCheckboxModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    TranslateModule,
+    MatPaginator,
+    MatTableModule,
+    MatCardModule,
+    MatSelectModule,
+    MatCheckboxModule
+  ],
   templateUrl: './rolesprevilages.component.html',
   styleUrls: ['./rolesprevilages.component.scss']
 })
 export class RolesprevilagesComponent implements OnInit, OnDestroy {
 
   formData: FormGroup;
+
   roleArray = [];
   parentMenu = [];
   actualData = [];
 
   dataSource: MatTableDataSource<any>;
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  displayedColumns: string[] = ['ext4', 'active', 'canAdd', 'canEdit', 'canDelete'];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator: MatPaginator;
+
+  displayedColumns: string[] = [
+    'ext4',
+    'active',
+    'canAdd',
+    'canEdit',
+    'canDelete'
+  ];
+
+  // Header checkbox states
+  activeChecked = false;
+  canAddChecked = false;
+  canEditChecked = false;
+  canDeleteChecked = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -42,15 +68,15 @@ export class RolesprevilagesComponent implements OnInit, OnDestroy {
     private spinner: NgxSpinnerService,
     private commonService: CommonService,
     route: ActivatedRoute
-
   ) {
     this.commonService.routeParam = route.snapshot.routeConfig.path;
+
     this.formData = this.formBuilder.group({
       role: [null],
       parentMenu: [null]
     });
-    this.formData.controls['parentMenu'].disable();
 
+    this.formData.controls['parentMenu'].disable();
   }
 
   ngOnInit() {
@@ -60,102 +86,177 @@ export class RolesprevilagesComponent implements OnInit, OnDestroy {
 
   GetRoles() {
     const getRolesUrl = this.apiConfigService.getRoles;
-    this.apiService.apiGetRequest(getRolesUrl).subscribe(
-      response => {
-        console.log(response);
-        const res = response;
-        if (res != null && res.status === StatusCodes.pass) {
-          if (res.response != null) {
-            this.roleArray = res.response['roles'];
-          }
+
+    this.apiService.apiGetRequest(getRolesUrl).subscribe(response => {
+      const res = response;
+
+      if (res != null && res.status === StatusCodes.pass) {
+        if (res.response != null) {
+          this.roleArray = res.response['roles'];
         }
-        this.spinner.hide();
-      });
+      }
+
+      this.spinner.hide();
+    });
   }
 
-  onRuleChange(data) {
+  onRuleChange(data: any) {
     this.reset();
+
     this.formData.patchValue({
       role: data.value
-    })
+    });
+
     this.formData.controls['parentMenu'].enable();
   }
 
   GetParentMenus() {
     const getRolesUrl = this.apiConfigService.getParentMenus;
-    this.apiService.apiGetRequest(getRolesUrl).subscribe(
-      response => {
-        const res = response;
-        if (res != null && res.status === StatusCodes.pass) {
-          if (res.response != null) {
-            this.parentMenu = res.response['parentMenus'];
-          }
+
+    this.apiService.apiGetRequest(getRolesUrl).subscribe(response => {
+      const res = response;
+
+      if (res != null && res.status === StatusCodes.pass) {
+        if (res.response != null) {
+          this.parentMenu = res.response['parentMenus'];
         }
-        this.spinner.hide();
-      });
+      }
+
+      this.spinner.hide();
+    });
   }
 
   selectedParentMenu() {
-    const getRolesUrl = [this.apiConfigService.getMenuList, this.formData.get('role').value, this.formData.get('parentMenu').value].join('/');
-    this.apiService.apiGetRequest(getRolesUrl).subscribe(
-      response => {
-        const res = response;
-        if (res != null && res.status === StatusCodes.pass) {
-          if (res.response != null) {
-            this.actualData = res.response.map(x => ({ ...x }));
-            this.dataSource = new MatTableDataSource(res.response.map(x => ({ ...x })));
-            this.dataSource.paginator = this.paginator;
-          }
+    const getRolesUrl = [
+      this.apiConfigService.getMenuList,
+      this.formData.get('role')?.value,
+      this.formData.get('parentMenu')?.value
+    ].join('/');
+
+    this.apiService.apiGetRequest(getRolesUrl).subscribe(response => {
+      const res = response;
+
+      if (res != null && res.status === StatusCodes.pass) {
+        if (res.response != null) {
+
+          this.actualData = res.response.map(x => ({ ...x }));
+
+          this.dataSource = new MatTableDataSource(
+            res.response.map(x => ({ ...x }))
+          );
+
+          this.dataSource.paginator = this.paginator;
+
+          this.updateHeaderCheckboxes();
         }
-        this.spinner.hide();
-      });
+      }
+
+      this.spinner.hide();
+    });
   }
 
-
-  checkboxCheck(event, column) {
-    if (!this.commonService.checkNullOrUndefined(this.dataSource)) {
-      this.dataSource.data = this.dataSource.data.map(val => {
-        val[column] = event.checked;
-        return val;
-      });
+  checkboxCheck(event: any, column: string) {
+    if (!this.dataSource?.data?.length) {
+      return;
     }
+
+    this.dataSource.data.forEach(row => {
+      row[column] = event.checked;
+    });
+
+    // Refresh table
+    this.dataSource.data = [...this.dataSource.data];
+
+    this.updateHeaderCheckboxes();
+  }
+
+  updateHeaderCheckboxes() {
+    if (!this.dataSource?.data?.length) {
+
+      this.activeChecked = false;
+      this.canAddChecked = false;
+      this.canEditChecked = false;
+      this.canDeleteChecked = false;
+
+      return;
+    }
+
+    this.activeChecked = this.dataSource.data.every(x => x.active);
+    this.canAddChecked = this.dataSource.data.every(x => x.canAdd);
+    this.canEditChecked = this.dataSource.data.every(x => x.canEdit);
+    this.canDeleteChecked = this.dataSource.data.every(x => x.canDelete);
   }
 
   save() {
-    let filterData = [];
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    const filterData = [];
+
     for (let d = 0; d < this.dataSource.data.length; d++) {
-      let filterValue = this.actualData.filter(res => res.operationCode == this.dataSource.data[d]['operationCode']);
+
+      const filterValue = this.actualData.filter(
+        res => res.operationCode === this.dataSource.data[d]['operationCode']
+      );
+
       if (filterValue.length) {
-        if (!this.commonService.IsObjectsMatch(filterValue[0], this.dataSource.data[d])) {
-          filterData.push(this.dataSource.data[d]);
+
+        if (!this.commonService.IsObjectsMatch(
+          filterValue[0],
+          this.dataSource.data[d]
+        )) {
+
+          const temp = { ...this.dataSource.data[d] };
+
+          temp['roleId'] = this.formData.get('role')?.value;
+          temp['userId'] = user.seqId;
+
+          filterData.push(temp);
         }
       }
     }
 
-    const getAccessUrl = [this.apiConfigService.giveAccess, this.formData.get('role').value].join('/');
+    const getAccessUrl = [
+      this.apiConfigService.giveAccess,
+      this.formData.get('role')?.value
+    ].join('/');
+
     this.apiService.apiPostRequest(getAccessUrl, filterData).subscribe(
       response => {
         const res = response;
+
         if (res != null && res.status === StatusCodes.pass) {
+
           if (res.response != null) {
-            this.alertService.openSnackBar(Static.LoginSussfull, Static.Close, SnackBar.success);
+
+            this.alertService.openSnackBar(
+              Static.LoginSussfull,
+              Static.Close,
+              SnackBar.success
+            );
+
             this.reset();
           }
         }
-        this.spinner.hide();
-      });
 
+        this.spinner.hide();
+      }
+    );
   }
 
   reset() {
     this.formData.reset();
+
+    this.activeChecked = false;
+    this.canAddChecked = false;
+    this.canEditChecked = false;
+    this.canDeleteChecked = false;
+
     this.dataSource = undefined;
+
     this.formData.controls['parentMenu'].disable();
   }
 
   ngOnDestroy(): void {
     this.commonService.routeParam = null;
   }
-
 }
-
