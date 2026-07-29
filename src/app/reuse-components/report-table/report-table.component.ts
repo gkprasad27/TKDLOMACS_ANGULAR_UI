c9b11fcf88eeb208e9e0b4386efda964d3eb7ecc
gkprasad27 <krishnaprasadg81@gmail.com>
2026-07-29 18:44:25 +0530

    changes

--- src/app/reuse-components/report-table/report-table.component.ts
+++ src/app/reuse-components/report-table/report-table.component.ts
@@ -1011,7 +1011,7 @@ export class ReportTableComponent implements OnInit, OnChanges {
             this.getTranslationKey(rightKey)
           );
 
-          valueRowData[columns.length - 1] =
+          valueRowData[2] =
             `${rightLabel}: ${headerObj[rightKey] ?? ''}`;
         }
 
